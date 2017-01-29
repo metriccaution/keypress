de8f427ad77309bbc31b9d4bb4a00b0b0b7d49d4
metriccaution <xang@hotmail.co.uk>
2017-01-29 10:48:34 +0000

    Making the styling more sane and mapping keycodes to printable strings more sensible

--- app.js
+++ app.js
@@ -28,7 +28,67 @@
 
   var store = keyStore();
 
+  function intervalBuilder() {
+    var handlers = [];
+
+    function map(char) {
+      var matches = handlers.filter(function(handler) {
+        return handler.range[0] <= char && handler.range[1] >= char;
+      });
+
+      if (matches.length === 0) {
+        return '';
+      }
+
+      return matches[0].mapper(char);
+    };
+
+    function addRange(start, stop, mapper) {
+      handlers.push({
+        range   : [start, stop],
+        mapper  : mapper
+      });
+    };
+
+    function singleValue(value, char) {
+      addRange(value, value, function() {
+        return char;
+      });
+    };
+
+    map.addRange = addRange;
+    map.singleValue = singleValue;
+
+    return map;
+  };
+
+  var charMap = intervalBuilder();
+
+  charMap.singleValue(8,   'Backspace');
+  charMap.singleValue(9,   'Tab');
+  charMap.singleValue(13,  'Enter');
+  charMap.singleValue(16,  'Shift');
+  charMap.singleValue(17,  'Ctrl');
+  charMap.singleValue(18,  'Left alt');
+  charMap.singleValue(20,  'Caps Lock');
+  charMap.singleValue(27,  'Esc');
+  charMap.singleValue(32,  'Space');
+  charMap.singleValue(33,  'Page Up');
+  charMap.singleValue(34,  'Page Down');
+  charMap.singleValue(35,  'End');
+  charMap.singleValue(36,  'Home');
+  charMap.singleValue(144, 'Num Lock');
+  charMap.singleValue(192, '`');
+  charMap.singleValue(225, 'Right alt');
+
+  charMap.addRange(48, 57, String.fromCharCode); // Number row
+  charMap.addRange(65, 90, String.fromCharCode); // Letters
+  charMap.addRange(96, 105, function(c) { // Numpad numbers
+    return 'Numpad ' + (c - 96);
+  });
+
   function renderKeys (events, element, allowedTypes) {
+
     var codeText = events.slice()
     .filter(function(e) {
       return allowedTypes.indexOf(e.type) !== -1;
@@ -37,8 +97,11 @@
       return e2.time - e1.time;
     })
     .map(function(e) {
-      var printable = String.fromCharCode(e.code);
-      return e.type + ':' + e.code + ' (' + printable + ')'
+      var printable = charMap(e.code);
+
+      return printable
+      ? e.type + ':' + e.code + ' (' + printable + ')'
+      : e.type + ':' + e.code;
     })
     .reduce(function(text, line) {
       return text + '\n' + line;
